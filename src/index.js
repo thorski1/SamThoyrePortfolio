9976cd26b17a6c9c9432b79ee952a22543ff7b8f
Sam Thoyre <sthoyre@gmail.com>
2019-08-17 15:08:59 -0400

    get gh-pages nonsense out of there so I can get this piece working on netlify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Landing from './components/landingpage'
 import './index.css';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
